93377a8754487462de06524a0317a0415de51acd
Roman Kaganov <q2mber@icloud.com>
2023-01-17 00:18:10 +0200

    fix

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
-    apiKey: "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",
+  apiKey: "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",
 });
 const openai = new OpenAIApi(configuration);
 
@@ -17,26 +17,26 @@ app.get('/', (req, res) => {
 })
 
 app.post('/ask', (req, res) => {
+  console.log("ASKED: ", req.body.data);
   makeRequest(req.body.data).then((data) => {
-    console.log(data.data.choices[0].text)
+    console.log('REPLY: ', data.data.choices[0].text)
     res.send(data.data.choices[0].text)
-});
+  }).catch((error)=>{
+    console.error(error)
+  })
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 
-
-
-
 async function makeRequest(data) {
-    const response = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: data,
-        temperature: 0,
-        max_tokens: 7,
-    });
-    return response
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: data,
+    temperature: 0,
+    max_tokens: 7,
+  });
+  return response
 }
 
 
